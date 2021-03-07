cf8faf3b2ee3ee44004a6a6e957f40db671b2961
tylerdowney13 <80125307+tylerdowney13@users.noreply.github.com>
2021-03-06 20:56:17 -0330

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -59,7 +59,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             return redirect(url_for('views.home'))
 
 
